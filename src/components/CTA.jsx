fbca45e0a071e40c0e82a93810dc8b37a55d50bb
Brian Battauz <brian.nbattauz@gmail.com>
2025-09-08 23:59:48 -0300

    Se termino la seccion de testimonios y la seccion del CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,13 +1,21 @@
 export default function CTA() {
   return (
-    <section className="py-16 px-8 bg-primary text-center">
-      <h2 className="text-3xl font-bold">Join Us to Make a Difference</h2>
-      <p className="mt-4">
-        Your small contribution can bring a huge change to someone's life.
-      </p>
-      <button className="mt-8 bg-white px-6 py-3 rounded font-semibold hover:bg-gray-100 transition-colors">
-        Donate Now
-      </button>
+    <section className="relative py-20 px-35 bg-[#00715D] text-white">
+      <div className="flex flex-row items-center justify-between">
+        {/* Texto */}
+        <div className="text-left">
+          <h2 className="font-[recoleta] text-7xl font-semibold leading-tight max-w-3xl">
+            Let’s Help Other With Your Charity
+          </h2>
+        </div>
+
+        {/* Botón */}
+        <div className="my-5">
+          <button className="bg-[#FDD65B] font-[objektiv] text-[#003B29] font-semibold px-8 py-5 hover:brightness-110 transition">
+            Donate Now
+          </button>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -1,11 +1,45 @@
+import { FaStar } from "react-icons/fa";
+import { TfiAngleLeft, TfiAngleRight } from "react-icons/tfi";
+
 export default function Testimonial() {
   return (
-    <section className="py-16 px-8 text-center">
-      <blockquote className="text-2xl italic text-gray-700 max-w-2xl mx-auto">
-        “This charity changed my life. Thanks to their support, I was able to access education
-        and build a better future.”
-      </blockquote>
-      <p className="mt-6 text-gray-900 font-semibold">— Sarah Johnson</p>
+    <section className="py-20 px-35 text-center">
+      <div className="relative bg-white rounded-l-[30%] flex w-full mb-20">
+        <div className="absolute bg-[#00715D] text-white w-30 h-30 flex items-center justify-center rounded-b-full z-10">
+          <FaStar className="text-yellow-500 text-xl mr-2" />
+          <span className="font-[recoleta] font-semibold text-xl"> 5.0</span>
+        </div>
+
+        <div className="absolute left-0 w-1/2 h-[600px] rounded-t-full">
+          <img
+            src="/testimonio1.png"
+            alt="testimonio"
+            className="w-full h-full object-cover object-top rounded-b-full bg-[#FDD65B]"
+          />
+        </div>
+
+        <div className="ml-auto w-1/2 p-21 flex flex-col justify-start">
+          <blockquote className="font-[objektiv] text-xl text-gray-500 leading-relaxed mb-15 text-left">
+            "We're very happy that the challenge went well, and we're grateful that we have a partner in Charity Challenge whom we could trust to take the best possible care of our supporters."
+          </blockquote>
+          <h3 className="font-[recoleta] text-3xl font-semibold not-italic text-left">
+            Mustafa Kamal
+          </h3>
+          <p className="font-[objektiv] font-semibold text-sm text-gray-600 text-left mt-2">
+            Ceo, Toogle
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-center my-10 gap-4">
+        <button className="flex items-center justify-center w-12 h-12 rounded-full hover:bg-[#FFEBA7] transition">
+          <TfiAngleLeft className="text-2xl text-gray-700 text-bold" />
+        </button>
+        <button className="flex items-center justify-center w-12 h-12 rounded-full hover:bg-[#FFEBA7] transition">
+          <TfiAngleRight className="text-2xl text-gray-700" />
+        </button>
+      </div>
+
     </section>
   );
 }
